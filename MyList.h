b183e30226bd759c24ee0bc8bbcee93e0b181170
Mathunywa <mnyuku21@gmail.com>
2019-02-25 17:51:39 +0000

    Mathunywa

---
+++ MyList.h
@@ -0,0 +1,17 @@
+#pragma once
+//list of all available fuctiions
+
+typedef  char* DataType;
+
+struct node 
+{ DataType data;
+  node* next;
+};
+
+typedef  node*  List;
+           
+void Init  (List&);
+bool IsEmpty (List );
+void  Insert (DataType, List&);
+void  Delete (DataType, List&); 
+void  ShowData (List);
